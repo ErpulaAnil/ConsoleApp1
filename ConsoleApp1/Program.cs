0a269e1a05286927a3986fa06be27c6249b674ac
aerpula <aerpula@IM-RT-LP-661>
2022-11-21 14:27:15 +0530

    now we have many messages

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! My name is anil one more modified");
+Console.WriteLine("Hello, World! My name is anil one more modified");
+Console.WriteLine("Hello, World! My name is anil one more modified");
+Console.WriteLine("Hello, World! My name is anil one more modified");
+Console.WriteLine("Hello, World! My name is anil one more modified");
+Console.WriteLine("Hello, World! My name is anil one more modified");
